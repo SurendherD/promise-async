bcc392ef29bd2a3485574562ff226d8590af88b0
SurendherD <144557309+SurendherD@users.noreply.github.com>
2024-04-22 10:49:10 +0530

    Initial commit

---
+++ promise.js
@@ -0,0 +1,38 @@
+const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
+const newCookie = {name:"Biscotti Cookies"};
+
+// Progression 1: create a function to get all the cookies
+function getallcookies(){
+  return cookies;
+}
+  // Progression 2: using setTimeout() -- use 1000 units for time parameter
+function getcookiesDelayed(){
+  setTimeout(()=>{
+    let cookies=getallcookies();
+    console.log(cookies);
+  },1000);
+}
+
+
+//Progression 3: Create a function to creat cookies and create an object of Promise.
+function createCookie(newCookie){
+  return new Promise((resolve,reject)=>{
+        // Progression 4: adding two parameters and using setTimeout() -- use 2000 units for time parameter
+    setTimeout(()=>{
+      // Progression 5: handling errors and adding new cookie to the list
+      cookies.push(newCookie);
+      const error = false;
+      if(!error){
+        resolve();
+      }
+      else{
+        reject('no cookies')
+      }
+    },2000);
+  }
+  );
+};
+
+// Progression 6: call function using `.then`
+createCookie(newCookie).then(getcookiesDelayed);
+
